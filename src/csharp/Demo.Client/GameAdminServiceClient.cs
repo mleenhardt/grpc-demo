8f299ea26748513e6f12c210cfb6a96f972e36d8
Mathieu Leenhardt <mathieu.leenhardt@gmail.com>
2016-02-02 00:42:30 +0100

    Some client implementation

---
+++ src/csharp/Demo.Client/GameAdminServiceClient.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Demo.Common.ServiceDefinition;
+
+namespace Demo.Client
+{
+    public sealed class GameAdminServiceClient
+    {
+        private readonly GameAdminService.IGameAdminServiceClient _grpcClient;
+
+        public GameAdminServiceClient(GameAdminService.IGameAdminServiceClient grpcClient)
+        {
+            _grpcClient = grpcClient;
+        }
+
+        public Task<Account> GetAccountAsync(int accountId)
+        {
+            return _grpcClient.GetAccountAsync(new AccountRequest { AccountId = accountId }).ResponseAsync;
+        } 
+    }
+}
--- src/csharp/Demo.Client/Program.cs
+++ src/csharp/Demo.Client/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Demo.Common.ServiceDefinition;
+using Grpc.Core;
 
 namespace Demo.Client
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var rpcChannel = new Channel("localhost:1337", ChannelCredentials.Insecure);
+            var rpcClient = new GameAdminServiceClient(GameAdminService.NewClient(rpcChannel));
+
+            rpcChannel.ConnectAsync().Wait();
+            Log($"GameAdminServiceClient connected to {rpcChannel.ResolvedTarget}, channel state = {rpcChannel.State}");
+
+
+            Log($"GameAdminServiceClient disconnecting from {rpcChannel.ResolvedTarget}, channel state = {rpcChannel.State}");
+            rpcChannel.ShutdownAsync().Wait();
+
+            Console.WriteLine("Press any key to stop the client...");
+            Console.ReadKey();
+        }
+
+        static void Log(string message)
+        {
+            Console.WriteLine($"{DateTime.UtcNow} -- {message}");
         }
     }
 }
