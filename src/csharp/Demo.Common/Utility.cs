d0eff179a1ec9fda0c024728afb98f12e0b04be6
Mathieu Leenhardt <mathieu.leenhardt@gmail.com>
2016-02-03 00:57:28 +0100

    Added Utility.GetRandomChatMessage

--- src/csharp/Demo.Common/Utility.cs
+++ src/csharp/Demo.Common/Utility.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Demo.Common.ServiceDefinition;
 
 namespace Demo.Common
 {
@@ -10,6 +11,17 @@ namespace Demo.Common
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             var random = new Random();
             return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static ChatMessage GetRandomChatMessage(int accountId)
+        {
+            int characterId = new Random().Next();
+            return new ChatMessage
+            {
+                CharacterId = characterId,
+                Message = $"This is a random test message from account {accountId}, characterId {characterId}",
+                TimestampUtc = DateTime.UtcNow.Ticks
+            };
         }
     }
 }
