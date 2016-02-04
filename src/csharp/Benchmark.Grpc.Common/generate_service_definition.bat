38f024e6dca0578bfaa9118eaf8a7701309dba51
Mathieu Leenhardt <mathieu.leenhardt@gmail.com>
2016-02-04 11:34:04 +0100

    Deleted benchmark projects, moved into a separate repo

--- src/csharp/Benchmark.Grpc.Common/generate_service_definition.bat
+++
@@ -1,2 +0,0 @@
-cd /d %~dp0
-..\packages\Google.Protobuf.3.0.0-alpha4\tools\protoc.exe -I..\..\protos --csharp_out .\ --grpc_out .\ --plugin=protoc-gen-grpc=..\packages\Grpc.Tools.0.12.0\tools\grpc_csharp_plugin.exe ..\..\protos\BenchmarkService.proto
